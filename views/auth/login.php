0e817a183e717745faf2883950a5779db18f1797
juanjopareja <jupg25@msn.com>
2023-10-28 21:44:45 +0200

    Add login form html

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,27 @@
-<h1>login</h1>
+<div class="container">
+    <h1>UpTask</h1>
+    <p>Crea y Administra tus proyectos</p>
+
+    <div class="container-sm">
+        <p class="description-page">Iniciar Sesión</p>
+
+        <form action="/" method="POST" class="form">
+            <div class="field">
+                <label for="email">Email</label>
+                <input type="email" id="email" placeholder="Tú Email" name="email">
+            </div>
+
+            <div class="field">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Tú Password" name="password">
+            </div>
+
+            <input type="submit" class="button" value="Iniciar Sesión">
+        </form>
+
+        <div class="actions">
+            <a href="/create">¿Aún no tienes una cuenta? Obtener Una</a>
+            <a href="/forget">¿Olvidaste tu Password?</a>
+        </div>
+    </div> <!--.container-sm -->
+</div>
