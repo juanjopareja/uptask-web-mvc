c490db6d89811d2cb7d69b66bcb6a3464d74711a
juanjopareja <jupg25@msn.com>
2023-10-28 18:31:00 +0200

    Add login views

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,26 +1,31 @@
 <?php
 
 namespace Controllers;
+use MVC\Router;
 
 class LoginController {
-    public static function login() {
-        echo "Desde login...";
-
+    public static function login(Router $router) {
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         }
+
+        $router->render('auth/login', [
+            'title' => 'Iniciar Sesión'
+        ]);
     }
     
     public static function logout() {
         echo "Desde logout...";
     }
 
-    public static function create() {
-        echo "Desde crear...";
-    
+    public static function create(Router $router) {
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
     
         }
+
+        $router->render('auth/create', [
+            'title' => 'Crea tu cuenta en UpTask'
+        ]);
     }
 
     public static function forget() {
