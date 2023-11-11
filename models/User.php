7c24c37965f6d758389d0fa0a2cc4635ac4c4044
juanjopareja <jupg25@msn.com>
2023-11-11 16:10:30 +0100

    Add password validation

--- models/User.php
+++ models/User.php
@@ -12,6 +12,7 @@ class User extends ActiveRecord {
         $this->name = $args['name'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+        $this->passwordRepeat = $args['passwordRepeat'] ?? '';
         $this->token = $args['token'] ?? '';
         $this->confirmed = $args['confirmed'] ?? '';
     }
@@ -23,6 +24,18 @@ class User extends ActiveRecord {
 
         if(!$this->email) {
             self::$alerts['error'][] = 'El email del usuario es obligatorio';
+        }
+
+        if(!$this->password) {
+            self::$alerts['error'][] = 'El password del usuario es obligatorio';
+        }
+
+        if(strlen($this->password) < 6) {
+            self::$alerts['error'][] = 'El password debe contener al menos 6 caracteres';
+        }
+
+        if($this->password !== $this->passwordRepeat) {
+            self::$alerts['error'][] = 'Los passwords son diferentes';
         }
 
         return self::$alerts;
